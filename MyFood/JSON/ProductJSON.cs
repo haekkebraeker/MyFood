d391c97793a0877483a2267ca4511145690e30b1
PaulaBras <68814660+PaulaBras@users.noreply.github.com>
2022-04-05 12:00:20 +0200

    small json

---
+++ MyFood/JSON/ProductJSON.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace MyFood.JSON
+{
+    class ProductJSON
+    {
+        [JsonProperty("EAN")]
+        public string EAN { get; private set; }
+        [JsonProperty("MHD")]
+        public DateTime MHD { get; private set; }
+        [JsonProperty("ID")]
+        public int ID { get; private set; }
+        [JsonProperty("Name")]
+        public string Name { get; private set; }
+
+    }
+}
